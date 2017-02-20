979e906915e6e16b7f6216924c64df8b6ff338dc
Maxim Pestryakov <maximpestryakov@gmail.com>
2017-02-20 20:03:47 +0300

    Initial commit

---
+++ src/main/java/Currency.java
@@ -0,0 +1,32 @@
+import java.io.InputStream;
+import java.util.Arrays;
+import java.util.InputMismatchException;
+import java.util.List;
+import java.util.Scanner;
+
+class Currency {
+
+    private static final List<String> CURRENCY_STINGS = Arrays.asList("AUD", "BGN", "BRL", "CAD", "CHF", "CNY", "CZK",
+            "DKK", "EUR", "GBP", "HKD", "HRK", "HUF", "IDR", "ILS", "INR", "JPY", "KRW", "MXN", "MYR", "NOK", "NZD",
+            "PHP", "PLN", "RON", "RUB", "SEK", "SGD", "THB", "TRY", "USD", "ZAR");
+
+    private int index = -1;
+
+    private Scanner scanner;
+
+    Currency(InputStream inputStream) {
+        scanner = new Scanner(inputStream);
+    }
+
+    void read() {
+        String currency = scanner.next().toUpperCase();
+        index = CURRENCY_STINGS.indexOf(currency);
+        if (index == -1) {
+            throw new InputMismatchException();
+        }
+    }
+
+    boolean hasCurrency() {
+        return index != -1;
+    }
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,25 @@
+import java.util.InputMismatchException;
+
+public class Main {
+    public static void main(String[] args) {
+        Currency base = new Currency(System.in);
+        Currency symbols = new Currency(System.in);
+
+        while (!base.hasCurrency()) {
+            System.out.println("Enter from currency:");
+            try {
+                base.read();
+            } catch (InputMismatchException e) {
+                System.out.println("Wrong format!");
+            }
+        }
+        while (!symbols.hasCurrency()) {
+            System.out.println("Enter to currency:");
+            try {
+                symbols.read();
+            } catch (InputMismatchException e) {
+                System.out.println("Wrong format!");
+            }
+        }
+    }
+}
